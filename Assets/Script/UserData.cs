c6a03e3744333305cebc3496e7aee00cf78440c3
StarCandy-D2 <kdh04090@gmail.com>
2025-06-04 13:57:31 +0900

    add userdata, UI

---
+++ Assets/Script/UserData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NewUserData", menuName = "User/User Data")]
+public class UserData : ScriptableObject
+{
+    public string userName = "D";
+    public int balance = 100000;
+    public int cash = 80000;
+}
+
---
+++ Assets/Script/UserDataDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UserDataDisplay : MonoBehaviour
+{
+    public UserData userData;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI balanceText;
+    public TextMeshProUGUI cashText;
+
+    void Start()
+    {
+        if (userData != null)
+        {
+            nameText.text = userData.userName;
+            balanceText.text = userData.balance.ToString("N0"); // 100,000 형식
+            cashText.text = userData.cash.ToString("N0");
+        }
+    }
+}
